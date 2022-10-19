eddae66c040e796d6eedcb189c314e042c4db823
Marco <marco.beer93@web.de>
2022-10-19 19:51:35 +0200

    added the main components and structure

---
+++ src/Goods/Goods.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Goods.scss';
+
+
+export const Goods = () => {
+    return (
+
+        <div className='goods-box'>
+            <div className='navbar'>
+                <div>Sortiment 1</div>
+                <div>Sortiment 2</div>
+                <div>Sortiment 3</div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Goods
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>FabMa Market</h1>
+        </div>
+    )
+}
+
+export default Header
+
+
